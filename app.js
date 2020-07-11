f007c5e5e050f3b13b128574a16d59eb88085759
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2020-07-11 15:41:37 +0530

    Add responsiveness

--- app.js
+++ app.js
@@ -143,24 +143,24 @@ exportBtn.addEventListener("click", (e) => {
 
 //Magically expanding textarea
 //Codepen reference for the magically expanding textarea: https://codepen.io/tomhodgins/pen/KgazaE
-var comfyText = (function () {
-  var tag = document.querySelectorAll("textarea");
-  for (var i = 0; i < tag.length; i++) {
-    tag[i].addEventListener("paste", autoExpand);
-    tag[i].addEventListener("input", autoExpand);
-    tag[i].addEventListener("keyup", autoExpand);
-  }
-  function autoExpand(e, el) {
-    var el = el || e.target;
-    el.style.height = "inherit";
-    el.style.height = el.scrollHeight + "px";
-  }
-  window.addEventListener("load", expandAll);
-  window.addEventListener("resize", expandAll);
-  function expandAll() {
-    var tag = document.querySelectorAll("textarea");
-    for (var i = 0; i < tag.length; i++) {
-      autoExpand(e, tag[i]);
-    }
-  }
-})();
+// var comfyText = (function () {
+//   var tag = document.querySelectorAll("textarea");
+//   for (var i = 0; i < tag.length; i++) {
+//     tag[i].addEventListener("paste", autoExpand);
+//     tag[i].addEventListener("input", autoExpand);
+//     tag[i].addEventListener("keyup", autoExpand);
+//   }
+//   function autoExpand(e, el) {
+//     var el = el || e.target;
+//     el.style.height = "inherit";
+//     el.style.height = el.scrollHeight + "px";
+//   }
+//   window.addEventListener("load", expandAll);
+//   window.addEventListener("resize", expandAll);
+//   function expandAll() {
+//     var tag = document.querySelectorAll("textarea");
+//     for (var i = 0; i < tag.length; i++) {
+//       autoExpand(e, tag[i]);
+//     }
+//   }
+// })();
